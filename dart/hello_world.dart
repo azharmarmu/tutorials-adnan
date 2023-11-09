971c5040c76911a177ecc0df25caebc2fc219257
azhar-msc <149996464+azhar-msc@users.noreply.github.com>
2023-11-09 21:18:17 +0530

    hello-world

---
+++ dart/hello_world.dart
@@ -0,0 +1,13 @@
+// void main() {
+//   //entry point for dart
+//   print('Hello World'); /*sdsds */
+// }
+
+//()-arg/parametr brackets
+//{}- flower/statement brackets
+//[]- List bracket/ of
+//<>- Generic bracket
+
+void main() {
+  print('Hello World');
+}
